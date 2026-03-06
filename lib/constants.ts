bb44c49f2a36c9e2a2d767faf8b1518c4c29131b
EchosV <voyagerechos@hotmail.com>
2026-03-07 02:38:23 +0800

    Create constants.ts

---
+++ lib/constants.ts
@@ -0,0 +1,16 @@
+export const DIFFICULTIES = [
+  { id: 'n5', label: 'N5' },
+  { id: 'n4', label: 'N4' },
+  { id: 'n3', label: 'N3' },
+  { id: 'n2', label: 'N2' },
+  { id: 'n1', label: 'N1' }
+] as const
+
+export const THEMES = [
+  { id: 'daily', label: '日常生活' },
+  { id: 'travel', label: '旅遊' },
+  { id: 'entertainment', label: '娛樂' },
+  { id: 'study', label: '學習' },
+  { id: 'business', label: '商業' },
+  { id: 'love', label: '戀愛' }
+] as const
